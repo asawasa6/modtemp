b44890b5df8715722b294323d3fe9c0a439e698e
asawasa6 <106855948+asawasa6@users.noreply.github.com>
2026-02-19 01:45:42 +0900

    add

---
+++ src/main/java/aircraft/Main.java
@@ -0,0 +1,12 @@
+package aircraft;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Main.MODID)
+public class Main {
+    public static final String MODID = "aircraft";
+
+    public Main(IEventBus modBus){
+    }
+}
