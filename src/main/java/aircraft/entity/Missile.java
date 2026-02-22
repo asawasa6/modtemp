3815cda812c479d5f50c0a27deea451e124cc67b
asawasa6 <106855948+asawasa6@users.noreply.github.com>
2026-02-22 20:07:22 +0900

    change

--- src/main/java/aircraft/entity/Missile.java
+++ src/main/java/aircraft/entity/Missile.java
@@ -1,16 +1,17 @@
 package aircraft.entity;
 
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
 
+import net.minecraft.world.phys.Vec3;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
-public class Missile extends Entity implements GeoEntity {
+public class Missile extends Projectile implements GeoEntity {
 
     private final AnimatableInstanceCache cache =
             GeckoLibUtil.createInstanceCache(this);
@@ -40,5 +41,62 @@ public class Missile extends Entity implements GeoEntity {
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return cache;
+    }
+
+    @Override
+    public void onAddedToLevel() {
+        super.onAddedToLevel();
+        this.setDeltaMovement(0,0.1,0);
+    }
+    @Override
+    public void tick() {
+        super.tick();
+
+        if(!this.level().isClientSide) {
+
+            double acceleration = 0.02;
+            double maxSpeed = 0.6;
+
+            double radius = 2.5;
+
+            Vec3 motion = this.getDeltaMovement();
+
+
+            double newY = motion.y + acceleration;
+
+            if(newY > maxSpeed) {
+                newY = maxSpeed;
+            }
+
+            this.setDeltaMovement(motion.x,newY,motion.z);
+
+
+            var enemies = this.level().getEntitiesOfClass(
+                    aircraft.entity.Enemy.class,
+                    this.getBoundingBox().inflate(radius)
+            );
+
+            if(!enemies.isEmpty()) {
+                explode();
+            }
+        }
+    }
+
+    private void explode() {
+        this.level().explode(
+                this,
+                this.getX(),
+                this.getY(),
+                this.getZ(),
+                3.0f,
+                Level.ExplosionInteraction.MOB
+        );
+
+        this.discard();
+    }
+
+    @Override
+    public boolean isNoGravity() {
+        return true;
     }
 }
